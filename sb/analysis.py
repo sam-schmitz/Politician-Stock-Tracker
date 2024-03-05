7e6a9e281ca35db061cd85de4c1c6922794f31a5
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-03-05 12:33:57 -0600

    created analysis.py and a rough draft of six_months_avg_mem

---
+++ sb/analysis.py
@@ -0,0 +1,24 @@
+# analysis.py
+# By: Sam Schmitz
+# methods for analyzing the data 
+
+from datetime import datetime, timedelta, date
+
+from server import stockBotAPI
+
+def six_months_avg_mem(mem):
+    api = stockBotAPI()
+    d = date.today()-timedelta(days=180)
+    trades = api.get_member_trades(mem, datetime(d.year, d.month, d.day))
+    totalGainPercentB = 0
+    totalGainPercentD = 0
+    for t in trades:
+        cPrice = t.cPrice()
+        totalGainPercentB += (cPrice/t.priceB)/t.priceB
+        totalGainPercentD += (cPrice/t.priceD)/t.priceD
+    avgGainB = totalGainPercentB/trades.length
+    avgGainD = totalGainPercentD/trades.length
+    print("Average % gained: ", avgGainB)
+    print("Average % gained after disclosed: ", avgGainD)
+
+
