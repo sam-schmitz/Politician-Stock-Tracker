ec27f0fb84914291c805010e90c0491c909c1ae0
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-03-05 10:06:05 -0600

    set up server.py for later coding

--- sb/server.py
+++ sb/server.py
@@ -4,7 +4,9 @@
 
 import sqlite3
 
-class stockBotDatabase:
+from trade import trade
+
+class stockBotAPI:
     
     def __init__(self):
         self.conn = sqlite3.connect('sbDatabase.db')
@@ -23,13 +25,39 @@ class stockBotDatabase:
         self.conn.close()
         print('SQLite connection closed')
         
-    def addTrade(self, trade):
+    def add_trade(self, trade):
         # trade is a trade obj
         self.cursor.execute(f"INSERT INTO trades VALUES ({trade.tick})")
         self.conn.commit()
         
-    def getAllTrades(self):
-        self.cursor.execute(f"SELECT * FROM trades")
+    def get_all_trades(self, date=None):
+        #gets trades from (current, date) for all members in the database
+        query = f"SELECT * FROM trades"
+        if date != None:    #add to query the date info
+            pass
+        self.cursor.execute(query)
         self.conn.commit()
-        return self.cursor.fetchall()
+        allTrades = self.cursor.fetchall()
+        return self._fetchall_to_trades(allTrades)
+    
+    def get_member_trades(self, member, date=None):
+        #gets trades from (current, date) for a named member
+        query = f""
+        if date != None:    #add to query the date info
+            pass
+        self.cursor.execute(query)
+        self.conn.commit()
+        allTrades = self.cursor.fetchall()
+        return self._fetchall_to_trades(allTrades)
+        
+    def _fetchall_to_trades(self, fetchall):
+        #turns data from fetchall into a list of trade obj
+        trades = []
+        for trade in fetchall:
+            pass
+            #make a trade object for each trade
+            #add it to trades
+        return trades
+        
+        
 
