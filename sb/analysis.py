97170ca8ae5c2185505306c4903903dd7881f7ba
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-03-06 10:45:26 -0600

    fixed an error in analysis.py

--- sb/analysis.py
+++ sb/analysis.py
@@ -7,15 +7,18 @@ from datetime import datetime, timedelta, date
 from server import stockBotAPI
 
 def six_months_avg_mem(mem):
+    #calculates the average percent the given member has made
+        #both since they made the trade and since they disclosed it
     api = stockBotAPI()
     d = date.today()-timedelta(days=180)
     trades = api.get_member_trades(mem, datetime(d.year, d.month, d.day))
     totalGainPercentB = 0
     totalGainPercentD = 0
     for t in trades:
-        cPrice = t.cPrice()
-        totalGainPercentB += (cPrice/t.priceB)/t.priceB
-        totalGainPercentD += (cPrice/t.priceD)/t.priceD
+        if t.saleType == "BUY":
+            cPrice = t.cPrice()
+            totalGainPercentB += (cPrice/t.priceB)/t.priceB
+            totalGainPercentD += (cPrice/t.priceD)/t.priceD
     avgGainB = totalGainPercentB/trades.length
     avgGainD = totalGainPercentD/trades.length
     print("Average % gained: ", avgGainB)
