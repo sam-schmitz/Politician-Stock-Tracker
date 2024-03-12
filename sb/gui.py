6b2d85852e3fbdc387dce1d363b5e668d5757431
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-03-12 10:24:10 -0500

    GUI frame created

--- sb/gui.py
+++ sb/gui.py
@@ -7,13 +7,90 @@ from tkinter import *
 from typing import Container
 
 from congressTrades import getTrades
+from fillDatabase import fill
 
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
+    
+import tkinter as tk
+from tkinter import ttk
+    
+class tkinterApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+        self.frames = {}
+        
+        for F in (StartPage, Page1, Page2):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(StartPage)
+        
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Startpage", font=("Verdana", 35))
+        label.grid(row=0, column=4, padx=10, pady=10)
+        
+        button1 = ttk.Button(self, text="Page 1",
+        command = lambda : controller.show_frame(Page1))
+        button1.grid(row=1, column=1, padx=10, pady=10)
+        
+        button2 = ttk.Button(self, text="Page 2",
+        command = lambda : controller.show_frame(Page2))
+        button2.grid(row=2, column=1, padx=10, pady=10)
+        
+class Page1(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 1", font=("Verdana", 35))
+        label.grid(row=0, column=4, padx=10, pady=10)
+        
+        button1 = ttk.Button(self, text="StartPage", 
+        command = lambda : controller.show_frame(StartPage))
+        button1.grid(row=1, column=1, padx=10, pady=10)
+        
+        button2 = ttk.Button(self, text="Page 2", 
+        command = lambda : controller.show_frame(Page2))
+        button2.grid(row=2, column=2, padx=10, pady=10)
+        
+        def getAllClicked():
+            d = date.today()-timedelta(days=3)
+            print("fill called with: ", datetime.now(), datetime(d.year, d.month, d.day))
+            #fill(datetime.now, datetime(d.year, d.month, d.day))
 
+        buttonGetAll = ttk.Button(self, text="Get All", command = getAllClicked)
+        buttonGetAll.grid(row=1, column=4, padx=10, pady=10)
+                
+        
+class Page2(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page 2", font=("Verdana", 35))
+        label.grid(row=0, column=4, padx=10, pady=10)
+        
+        button1 = ttk.Button(self, text="Page 1", 
+        command = lambda : controller.show_frame(Page1))
+        button1.grid(row=1, column=1, padx=10, pady=10)
+        
+        button2 = ttk.Button(self, text="Startpage", 
+        command = lambda : controller.show_frame(StartPage))
+        button2.grid(row=2, column=2, padx=10, pady=10)
+        
 if __name__ == "__main__":
-    #set up the tkinter window
+    app = tkinterApp()
+    app.mainloop()
+    """#set up the tkinter window
     root = Tk()
     root.title("Politician Stock Tracker")
     root.geometry('700x400')
@@ -52,32 +129,5 @@ if __name__ == "__main__":
     lblGetOne = Label(root, text="")
     lblGetOne.grid(column=0, row=3)
 
-    root.mainloop() # runs root
-    
-import tkinter as tk
-from tkinter import ttk
-    
-class tkinterApp(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.gris_columnconfigure(0, weight=1)
-        
-        self.frames = {}
-        
-        for F in (StartPage, Page1, Page2):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame(StartPage)
-        
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-        
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        pass
+    root.mainloop() # runs root"""
         
